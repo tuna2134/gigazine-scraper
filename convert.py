acfff9ba60562531aa28336e13ac929f1174cec4
コマリン親衛隊 <masato190411@icloud.com>
2024-08-02 11:58:10 +0000

    fixed

--- convert.py
+++ convert.py
@@ -6,8 +6,16 @@ with open("data.json", "r") as f:
     data = json.load(f)["contents"]
 
 
+labels = []
+
+
 with open("data.csv", "w") as f:
     writer = csv.DictWriter(f, ["title", "label"])
     writer.writeheader()
     for d in data:
+        if d["label"] not in labels:
+            labels.append(d["label"])
+        d["label"] = labels.index(d["label"])
         writer.writerow(d)
+
+print(labels)
