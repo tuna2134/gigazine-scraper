9b87aa9faf54fd26c28875afaa3787370a59e0ff
コマリン親衛隊 <masato190411@icloud.com>
2024-08-02 09:47:44 +0000

    fix

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ data = []
 url = "https://gigazine.net/"
 for i in range(100):
     articles, url = get_articles(url)
+    print(url)
     data.extend([{"title": article.title, "label": article.label} for article in articles])
     time.sleep(1)
 
